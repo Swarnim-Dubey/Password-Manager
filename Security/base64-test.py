3368e17eb3f43be812f86e1939fd47ae2951cf44
unknown <swarnimdubey135@gmail.com>
2025-12-21 00:58:05 +0530

    commit

--- Security/base64-test.py
+++ Security/base64-test.py
@@ -14,7 +14,29 @@ def decode_b64(txt: str) -> bytes:
         b = base64.b64decode(txt.encode("utf-8"))
         return b
     except Exception as e:
-        print("decode error:", e)
+        print(f"decode error : {e}")
         return b""
+    
+print("-" * 20, "BASE64 TEST (TESTING_PHASE)", "-" * 20)
+print()
 
+while True:
+    print("\n What you want to do?")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Exit")
 
+    ch = int(input("Enter choice : "))
+
+    if ch == 1:
+        s = input("Enter the text top encode : ")
+        out = encode_b64(s.encode("utf-8"))
+        print(f"Encoded text = {out}")
+
+    elif ch == 2:
+        s = input("Enter the text top encode : ")
+        result = decode_b64(s)
+        try:
+            print(f"Decode text = {result.decode("utf-8")}")
+        except:
+            ...
