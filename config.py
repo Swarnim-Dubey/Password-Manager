a2361b65281aaa0893fb8984e49c5de85f994c63
unknown <swarnimdubey135@gmail.com>
2025-12-28 16:36:20 +0530

    configurations of the project

---
+++ config.py
@@ -0,0 +1,14 @@
+# App information
+
+
+APP_NAME = "Password Manager"
+APP_VERSION = "1.0"
+
+# Encoding
+TEXT_ENCODING = "utf-8"
+
+# Database
+DATABASE_NAME = "password-manager.db"
+
+# Security
+AES_KEY_SIZE = 32  # AES-256 uses 32-byte keys
