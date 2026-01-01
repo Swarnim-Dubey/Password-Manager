4e41bad009bac21b849a10027f0526f9e62301da
unknown <swarnimdubey135@gmail.com>
2026-01-01 21:24:34 +0530

    updated main

--- main.py
+++ main.py
@@ -29,4 +29,80 @@ def login():
     pwd = input("Enter MASTER password : ")
 
     if check_password(pwd, stored_hash):
-        ...
+        print("Login Successful ✅")
+        return make_key(pwd)
+    else:
+        print("Wrong Password ❌")
+        return None
+
+# ----------- MENU ----------
+
+def dashboard(key):
+    while True:
+        print("---------- DASHBOARD ----------")
+        print("1. Add Credential")
+        print("2. View Credentials")
+        print("3. Delete Credential(s)")
+        print("4. Exit")
+
+        choice = int(input("Enter your choice : "))
+
+        if choice == 1:
+            service = input("Service Name : ")
+            username = input("Username : ")
+            password = input("Password : ")
+
+            encrypted = encrypt_text(password, key)
+            encrypted_b64 = encode_b64(encrypted)
+
+            add_credential(service, username, encrypted_b64)
+            print("Credential Stored Successfully! ✅")
+
+        elif choice == 2:
+            creds = get_credentials()
+
+            if not creds:
+                print("No credentials found 😓")
+                continue
+
+            print("\nStored Credentials are :")
+            for service, username, password_b64 in creds:
+                decrypted = decrypt_text(
+                    decode_b64(password_b64),
+                    key
+                )
+                print(f"{service} | {username} | {decrypted}")
+
+        elif choice == 3:
+            service = input("Service name to delete: ")
+            delete_credential(service)
+            print("Credential Deleted Successfully! 👍")
+
+        elif choice == 4:
+            print("Bye 👋")
+            break
+
+        else:
+            print("Invalid choice!")
+
+
+# ---------- PROGRAM START ----------
+
+def main():
+    print("\n","-" * 30, "PASSWORD MANAGER", "-" * 30)
+
+    init_db()
+
+    # First run
+    if not get_master_hash():
+        setup_master_password()
+
+    key = login()
+    if key is None:
+        sys.exit()
+
+    dashboard(key)
+
+
+if __name__ == "__main__":
+    main()
